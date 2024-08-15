4f28d62fec5994c4e780b8d13f7a8a06b2d8126d
Manthan997 <manthanparsekar997@gmail.com>
2024-08-16 00:21:51 +0530

    chaos

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -13,8 +13,8 @@ int main() {
     bot.walk(10);
     bot.current_position();
 
-    Pose start_pos = { Dir::A, 10, 3 };
-    std::vector<std::variant<Dir, float>> commands = { Dir::A, 10.0f, Dir::C, 12.4f};
+    Pose start_pos = { Dir::A, 0, 0 };
+    std::vector<std::variant<Dir, float>> commands = { Dir::A, 10.0f, Dir::B, 10.0f, Dir::C, 10.0f, Dir::D, 10.0f, Dir::E, 10.0f, Dir::F, 10.0f, Dir::A };
     bot.predict(start_pos, commands);
 
     bot.dump_commands();
